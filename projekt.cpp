426376e19f1db6b512256dfabe586cc6d7a7d875
mateuszumk <mateuszratkowski@mat.umk.pl>
2024-12-23 15:13:51 +0100

    dodano funkcje sprawdzajaca pierwszosc

--- projekt.cpp
+++ projekt.cpp
@@ -2,10 +2,24 @@
 
 using namespace std;
 
+bool czyPierwsza(int n) {
+	if (n < 2) return false;
+	for (int i = 2; i * i <= n; ++i) {
+		if (n % i == 0) return false;
+	}
+	return true;
+}
+
 int main() {
 	int n;
 	cin >> n;
 	cout << "wczyana liczba to: " << n << endl;
+	if (czyPierwsza(n)) {
+		cout << "wczytana liczba jest pierwsza" << endl;
+	}	else {
+		cout << "wczytana liczba nie jest pierwsza" << endl;
+	}
+
 
 	return 0;
 }
