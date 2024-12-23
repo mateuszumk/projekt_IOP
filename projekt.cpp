879d78216b00c47b8b26bde37ec9be25176ae056
mateuszumk <mateuszratkowski@mat.umk.pl>
2024-12-23 15:23:00 +0100

    dodano obsluge bledow

--- projekt.cpp
+++ projekt.cpp
@@ -3,23 +3,28 @@
 using namespace std;
 
 bool czyPierwsza(int n) {
-	if (n < 2) return false;
-	for (int i = 2; i * i <= n; ++i) {
-		if (n % i == 0) return false;
-	}
-	return true;
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; ++i) {
+                if (n % i == 0) return false;
+        }
+        return true;
 }
 
 int main() {
-	int n;
-	cin >> n;
-	cout << "wczyana liczba to: " << n << endl;
-	if (czyPierwsza(n)) {
-		cout << "wczytana liczba jest pierwsza" << endl;
-	}	else {
-		cout << "wczytana liczba nie jest pierwsza" << endl;
+        int n;
+        cin >> n;
+	if (n <= 0) {
+		cout << "podana liczba nie jest dodatnia" << endl;
+		return 1;
 	}
+	cout << "wczytana liczba to: " << n << endl;
+        if (czyPierwsza(n)) {
+                cout << "wczytana liczba jest pierwsza" << endl;
+        }       else {
+                cout << "wczytana liczba nie jest pierwsza" << endl;
+        }
 
 
-	return 0;
+        return 0;
 }
+
